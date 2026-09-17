b24dfe334a2d7c5ed506f4171dbdb41163aa8c79
Michaela Havlíková <michelle.hav17@gmail.com>
2026-09-17 23:04:06 +0200

    chore: scaffold Vite + React + TypeScript project
    Set up build tooling (Vite, TypeScript, ESLint, Tailwind) and app entry point.

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Layout } from "./components/Layout"
+import { About } from "./components/sections/About"
+import { Contact } from "./components/sections/Contact"
+import { Experience } from "./components/sections/Experience"
+import { Hero } from "./components/sections/Hero"
+import { Projects } from "./components/sections/Projects"
+import { Values } from "./components/sections/Values"
+
+
+function App() {
+  return (
+    <Layout title="Portfolio">
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Values />
+      <Contact />
+    </Layout>
+  )
+}
+
+export default App
